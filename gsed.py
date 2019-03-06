658cf543abf7441771a14325ba63cec59871ffb2
SLeRest <sebastienlerest22@gmail.com>
2019-03-06 03:05:53 +0100

    parseopt and process_file

--- gsed.py
+++ gsed.py
@@ -1,35 +1,34 @@
+#!/usr/bin/python
+
 import sys
 import os
 import unittest
+from optparse import OptionParser
 
 def print_help():
     print("help", flush=True)
 
 class Err():
-    def not_enouth_args():
-        print("Error: gsed: need at least 2 args",
-              file=sys.stderr, flush=True)
-        print_help()
-        sys.exit(1)
-
     def path_not_found(path):
         print("Error: gsed: Path doesn't exist:", path,
               file=sys.stderr, flush=True)
 
+    def cant_open_file(e):
+        print("Error: gsed: ", e, file=sys.stderr, flush=True)
+
 
 class Gsed:
     # reduction de ram
-    __slots__ = 'search', 'replace', 'files', 'dirs'
+    __slots__ = 'search', 'replace', 'files', 'dirs', 'options'
 
-    def __init__(self, args):
-        if len(args) < 2:
-            Err.not_enouth_args()
+    def __init__(self, args, options):
         self.search = ""
         self.replace = ""
         self.files = []
         self.dirs = []
+        self.options = options
         is_path = False
-        for arg in args[1:]:
+        for arg in args:
             if len(self.search) == 0:
                 self.search = arg
             elif len(self.replace) == 0:
@@ -49,8 +48,40 @@ class Gsed:
 
     # debug print
     def print(self):
-        print("gsed: search:\"", self.search, "\"replace:\"",
-              self.replace, "\"files:", self.files, "\"dirs:", self.dirs)
+        print("gsed: search:\"", self.search, "\"replace:\"",self.replace,"\"")
+        print("files:\"", self.files, "\"dirs:", self.dirs)
+        print("options:", self.options)
 
-gsed = Gsed(sys.argv)
-gsed.print()
+    def process_file(self):
+        for file in self.files:
+            with open(file, "r+") as f:
+                content = f.read()
+                content = content.replace(self.search, self.replace)
+                f.seek(0)
+                f.write(content)
+                f.truncate()
+                f.close()
+
+    def process(self):
+        self.process_files()
+#       self.process_dirs() TODO
+
+def main():
+    parser = OptionParser(usage="usage: %prog [options] search replace",
+                          version="%prog 1.0")
+    parser.add_option("-R", "--recursive",
+                      action="store_true",
+                      dest="recursive_flag",
+                      default=False,
+                      help="search in file's subdirectories recursively")
+    (options, args) = parser.parse_args()
+
+    if len(args) < 2:
+        parser.error("wrong number of arguments")
+    gsed = Gsed(args, options)
+    #gsed.print()
+    #gsed.process()
+
+
+if __name__ == '__main__':
+    main()
